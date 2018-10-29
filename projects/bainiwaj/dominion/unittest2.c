c99a3866c4b614ae2748c561957338962a40dd3e
Jaspal Bainiwal <bainiwaj@flip2.engr.oregonstate.edu>
2018-10-28 17:51:13 -0700

    updated unit test 2

--- projects/bainiwaj/dominion/unittest2.c
+++ projects/bainiwaj/dominion/unittest2.c
@@ -1,9 +1,9 @@
 /******************************************************************************
- *  * * Jaspal Bainiwal
- *   * * I am using the testCoinsUpdate template from assignment 3 assistance(2).
- *    * *I am testing the endTurn function in this test.
- *    ************************************************************************
- *    */
+ * * Jaspal Bainiwal
+ * * I am using the testCoinsUpdate template from assignment 3 assistance(2).
+ * *I am testing the endTurn function in this test.
+************************************************************************
+*/
 
 #include "dominion.h"
 #include "dominion_helpers.h"
@@ -11,22 +11,7 @@
 #include <stdio.h>
 #include <assert.h>
 #include "rngs.h"
-
-/*Wrote my own assert function because the helpful hint suggested that 
- *  * the built in assert might crash the program*/
-int myAssertFunction(int x, int y) 
-{
-	if (x != y)
-	{
-		printf("TEST FAILED\n");
-		return 0;
-	}
-	else
-	{
-		printf("TEST SUCCESSFULLY COMPLETED\n");
-		return 1;
-	}
-}
+int myAssertFunction(int x, int y);
 
 int main() {
 	int seed = 1000;
@@ -40,7 +25,6 @@ int main() {
 	
 	initializeGame(numPlayer, k, seed, &G); // initialize a new game
 	printf("TESTING endTurn():\n");
-	
 	printf("gamestate whoseTurn is 0 and after endTurn it should be 1\n");
 	endTurn(&G);
 	call = G.whoseTurn;
@@ -49,7 +33,6 @@ int main() {
 	{
 		counter++;
 	}
-
 	printf("If last players turn has ended it should go to player 0\n");
 	G.whoseTurn = 1;
 	endTurn(&G);
@@ -59,7 +42,6 @@ int main() {
 	{
 		counter++;
 	}
-
 	printf("Test if function resets card count\n");
 	G.playedCardCount = 3;
 	endTurn(&G);
@@ -69,7 +51,6 @@ int main() {
 	{
 		counter++;
 	}
-
 	printf("Test if hand count resets after turn ends\n");
 	G.whoseTurn = 0;
 	G.handCount[0] = 2;
@@ -80,7 +61,6 @@ int main() {
 	{
 		counter++;
 	}
-
 	printf("Test if outpost in play resets after turn ends\n");
 	G.outpostPlayed = 1;
 	endTurn(&G);
@@ -90,7 +70,6 @@ int main() {
 	{
 		counter++;
 	}
-
 	printf("Test if number of buys goes to one\n");
 	G.numBuys = 4;
 	endTurn(&G);
@@ -100,7 +79,6 @@ int main() {
 	{
 		counter++;
 	}
-
 	if (counter == 6)
 	{
 		printf("All tests passed!\n");
@@ -110,4 +88,19 @@ int main() {
 		printf("Not all tests passed!\n");
 	}
 	return 0;
+}
+/*Wrote my own assert function because the helpful hint suggested that
+ * the built in assert might crash the program*/
+int myAssertFunction(int x, int y)
+{
+	if (x != y)
+	{
+		printf("TEST FAILED\n");
+		return 0;
+	}
+	else
+	{
+		printf("TEST SUCCESSFULLY COMPLETED\n");
+		return 1;
+	}
 }
