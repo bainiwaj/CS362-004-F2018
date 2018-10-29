141d602a0f9766bd782f62fc509acfd82f3e126c
Jaspal Bainiwal <bainiwaj@flip2.engr.oregonstate.edu>
2018-10-28 17:50:49 -0700

    updated unit test 1

--- projects/bainiwaj/dominion/unittest1.c
+++ projects/bainiwaj/dominion/unittest1.c
@@ -1,8 +1,8 @@
 /* 
- *  * * Jaspal Bainiwal
- *   * * I am using the testCoinsUpdate template from assignment 3 assistance(2).
- *    * *I am testing the scoreFor function in this test.
- *     */
+ * * Jaspal Bainiwal
+ * * I am using the testCoinsUpdate template from assignment 3 assistance(2).
+ * *I am testing the scoreFor function in this test.
+ */
 
 #include "dominion.h"
 #include "dominion_helpers.h"
@@ -10,22 +10,7 @@
 #include <stdio.h>
 #include <assert.h>
 #include "rngs.h"
-
-/*Wrote my own assert function because the helpful hint suggested that 
- *  * the built in assert might crash the program*/
-int myAssertFunction(int x, int y) 
-{
-	if (x != y)
-	{
-		printf("TEST FAILED\n");
-		return 0;
-	}
-	else
-	{
-		printf("TEST SUCCESSFULLY COMPLETED\n");
-		return 1;
-	}
-}
+int myAssertFunction(int x, int y);
 
 int main() {
 	int seed = 1000;
@@ -40,7 +25,6 @@ int main() {
 
 	initializeGame(numPlayer, k, seed, &G); // initialize a new game
 	printf("TESTING scoreFor():\n");
-	
 	printf("Testing if player 1 starts off with hand score of zero\n");
 	G.handCount[0] = 0;
 	call = scoreFor(0, &G);
@@ -49,7 +33,6 @@ int main() {
 	{
 		counter++;
 	}
-
 	printf("Testing if player 1 hand has correct score when handcout is 2\n");
 	G.handCount[0] = 2;
 	G.hand[0][0] = province;
@@ -60,7 +43,6 @@ int main() {
 	{
 		counter++;
 	}
-
 	printf("Testing if player 2 hand has correct score with handcount 1\n");
 	G.handCount[1] = 1;
 	G.hand[1][0] = great_hall;
@@ -70,7 +52,6 @@ int main() {
 	{
 		counter++;
 	}
-
 	printf("Testing if player 1 starts off with discard pile score of zero\n");
 	G.handCount[0] = 0;
 	G.discardCount[0] = 0;
@@ -80,7 +61,6 @@ int main() {
 	{
 		counter++;
 	}
-
 	printf("Testing if player 2 starts off with discard pile score of zero\n");
 	G.handCount[1] = 0;
 	G.discardCount[1] = 0;
@@ -90,7 +70,6 @@ int main() {
 	{
 		counter++;
 	}
-
 	printf("Testing if player 1 DISCARD AND DECK score is the expected amount\n");
 	G.handCount[0] = 0;
 	G.discardCount[0] = 1;
@@ -102,8 +81,6 @@ int main() {
 	{
 		counter++;
 	}
-
-
 	if (counter == 6)
 	{
 		printf("All tests passed!\n");
@@ -114,4 +91,18 @@ int main() {
 	}
 	return 0;
 }
-
+/*Wrote my own assert function because the helpful hint suggested that
+ * the built in assert might crash the program*/
+int myAssertFunction(int x, int y)
+{
+	if (x != y)
+	{
+		printf("TEST FAILED\n");
+		return 0;
+	}
+	else
+	{
+		printf("TEST SUCCESSFULLY COMPLETED\n");
+		return 1;
+	}
+}
